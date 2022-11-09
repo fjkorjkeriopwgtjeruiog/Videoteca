5064b7a879f7ec321c93c6434b5f1a60e4826370
unknown <yix94091@educastur.es>
2022-11-09 15:36:14 +0100

    Prové algunas cosas de services

--- LucaSteam/src/services/ColeccionJuegos.java
+++ LucaSteam/src/services/ColeccionJuegos.java
@@ -1,12 +1,27 @@
 package services;
 
+import utilidades.Datos;
+import excepciones.StockException;
 import model.Juego;
 
 public class ColeccionJuegos {
 
 	private data.ColeccionJuegos coleccion = new data.ColeccionJuegos();
 
-	public boolean AddJuego(Juego j) throws StockException {
-        
+	public boolean AddJuego(Juego j) {
+        return this.AddJuego(j);
+    }
+	
+	public boolean eliminarJuego() {
+        int codigo = Datos.recogeInt("Indica el código del juego a eliminar");
+
+        if (stock.existeCodigo(codigo)) {
+            stock.eliminarProducto(codigo);
+            System.out.println("Juego eliminado");
+            return true;
+        } else {
+        	throw new Exception("El juego no tiene ese producto");
+            //return false;
+        }
     }
 }
