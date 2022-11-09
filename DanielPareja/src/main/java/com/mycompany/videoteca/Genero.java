d513caf7aa90d9a4f96d07036eb6dbe66fbb67a5
Daniel <danielpareja1992@gmail.com>
2022-11-09 12:58:29 +0100

    Enumerados
    Los enumerados de Plataforma y Genero.

---
+++ DanielPareja/src/main/java/com/mycompany/videoteca/Genero.java
@@ -0,0 +1,12 @@
+package com.mycompany.videoteca;
+
+public enum Genero{
+    plataformas,
+    rpg,
+    accion,
+    lucha,
+    carrera,
+    estrategia,
+    terror,
+    tiros;
+}
---
+++ DanielPareja/src/main/java/com/mycompany/videoteca/Videoteca.java
@@ -0,0 +1,10 @@
+package com.mycompany.videoteca;
+
+public class Videoteca{
+
+    public static void main(String[] args){
+        Plataforma tri=Plataforma.n64;
+        Genero mario=Genero.plataformas;
+        System.out.println("Mario 64 es de "+tri+" y salio para "+mario+".");
+    }
+}
