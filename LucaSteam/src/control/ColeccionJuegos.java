4c294e6232fb70fa987d5e6d7a6e78d13302a8a4
Ivan Blanco <asassdadasdaddfgdfg@gmail.com>
2022-11-09 14:22:37 +0100

    Ivan segundo commit no salio la clase juego coleccion

--- LucaSteam/src/control/ColeccionJuegos.java
+++ LucaSteam/src/control/ColeccionJuegos.java
@@ -2,4 +2,28 @@ package control;
 
 public class ColeccionJuegos {
 
+	public static void menu () {
+		boolean continuar = false; //posibilidad de salir del loop
+		do {	
+		boolean opcion1activa= false;
+	    System.out.println("Esto es el mensaje incial no corresponde a este spring");
+		int opcion=0; //la opcion que se elije
+		System.out.println("Presiona 1 para hacer una carga incial de juegos");
+		opcion = Entrada.entradaInt();
+		if (opcion==1) {
+			control.CSV.cargaInicialdeDatos();
+			opcion1activa= true;
+			
+		}
+		
+		//otras opciones
+			
+		} while (continuar);
+	}
+
+	public static void nuevojuego() {
+		services.ColeccionJuegos.nuevojuego();
+	}
+
+
 }
