8204bec5b6c05b71e9d7a7ee700657238eabb55b
Ivan Blanco <asassdadasdaddfgdfg@gmail.com>
2022-11-09 15:48:37 +0100

    Resolver otro error clase eliminar juego

--- LucaSteam/src/services/ColeccionJuegos.java
+++ LucaSteam/src/services/ColeccionJuegos.java
@@ -6,18 +6,6 @@ public class ColeccionJuegos {
 
 	private data.ColeccionJuegos coleccion = new data.ColeccionJuegos();
 	
-	public boolean eliminarJuego() {
-        int codigo = Datos.recogeInt("Indica el código del juego a eliminar");
-
-        if (stock.existeCodigo(codigo)) {
-            stock.eliminarProducto(codigo);
-            System.out.println("Juego eliminado");
-            return true;
-        } else {
-        	throw new Exception("El juego no tiene ese producto");
-            //return false;
-        }
-=======
 	public static boolean AddJuego(Juego j) {
         
 		
