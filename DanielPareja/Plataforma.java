930beab06766364451c3a07a006693757b0b2626
Daniel <danielpareja1992@gmail.com>
2022-11-10 18:06:23 +0100

    quitar txt vacios

--- DanielPareja/Plataforma.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.videoteca;
-
-public enum Plataforma{
-    atari2600,jaguar,
-    nes,snes,n64,gamecube,wii,
-    playstation,ps2,ps3,
-    mastersystem,megadrive,saturn,dreamcast,
-    xbox,xbox360;
-}
